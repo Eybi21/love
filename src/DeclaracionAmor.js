209ee6cb58c1d0ddaf6fefde9276cdffbd6c7046
Eybi Imanol Suy Guzmán <ESuy@launion.com.gt>
2025-03-30 20:56:11 -0600

    Agregar imagen

--- src/DeclaracionAmor.js
+++ src/DeclaracionAmor.js
@@ -7,21 +7,28 @@ const DeclaracionAmor = () => {
     return (
 
         <div className="flex justify-center items-center h-screen  text-center">
-            
-            <box>
+
+
             <div className="bg-white p-6 rounded-2xl shadow-xl">
-                
+
                 <h1 className="text-red-500 text-2xl font-bold">Hola, mi negrita ❤️</h1>
                 <p className="text-lg mt-2">Quería decirle algo especial...</p>
-                <button 
+                <button
                     className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-pink-400 transition"
                     onClick={() => setMostrar(true)}
                 >
                     Haz clic aquí
                 </button>
-                {mostrar && <p className="text-xl mt-4 text-pink-600">La amo con todo mi corazón y la aprecio mucho💖</p>}
+                {mostrar && (
+                    <>
+                        <p className="text-xl mt-4 text-pink-600">La amo con todo mi corazón y la aprecio mucho💖</p>
+                        <img src="/img/Collage dl.png" alt="Imagen amorosa" />
+                    </>
+                )
+
+
+                }
             </div>
-            </box>
         </div>
     );
 };
